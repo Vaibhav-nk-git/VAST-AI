02f638eb29c69dca0299d1469cac2fe94f447f40
Vaibs1 <92681528+Vaibs1@users.noreply.github.com>
2024-10-07 15:09:26 +0530

    Add global type declarations for PrismaClient

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined; // Adding this declaration globally
-}
-
 const prismadb = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") {
